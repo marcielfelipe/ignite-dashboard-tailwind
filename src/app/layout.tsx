dc31c7a50b79d0686614dd6e7d1fdc731bc257ed
marcielfelipe <marciel.felipe@outlook.com>
2023-08-29 19:17:10 -0300

    first-commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Ignite | Dashboard',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-br">
+      <body className={inter.className}>
+        <div>
+          <main className="min-h-screen w-full">{children}</main>
+        </div>
+      </body>
+    </html>
+  )
+}
